fb4fa113eb99687c028233e05124330931b9815e
server4001 <server4001@gmail.com>
2018-10-16 19:33:39 -0700

    Adds install of hashicorp vault

--- provision/common.sh
+++ provision/common.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 
-if [ ! -f /etc/yum.repos.d/epel.repo ]; then
-  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+if [ ! -f /usr/bin/man ]; then
+  yum install -y man man-pages
 fi
 
-if [ ! -f /usr/bin/redis-server ]; then
-  yum install -y redis
+if [ ! -f /etc/yum.repos.d/epel.repo ]; then
+  rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
 fi
 
 if [ ! -f /usr/bin/vim ]; then
   yum install -y vim-enhanced
 fi
-
-if [ ! -d /var/run/redis ]; then
-  mkdir -p /var/run/redis
-  chown redis:redis /var/run/redis
+if [ ! -f /usr/bin/repoquery ]; then
+  yum install -y yum-utils
 fi
-
-# TODO : Copy configs.
-
-if [ "$(pgrep -f /usr/bin/redis-server | wc -l)" -lt 1 ]; then
-  service redis start
-fi
-if [ "$(pgrep -f /usr/bin/redis-sentinel | wc -l)" -lt 1 ]; then
-  service redis-sentinel start
+if [ ! -f /usr/bin/unzip ]; then
+  yum install -y unzip
 fi
--- provision/first.sh
+++ provision/first.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+vault_filename='vault_0.11.3_linux_amd64.zip'
+vault_binary='/usr/bin/vault'
+
 if [ "$(grep -c second.gluster.loc /etc/hosts)" -lt 1 ]; then
   echo "192.168.35.55 second second.gluster.loc" >> /etc/hosts
 fi
@@ -8,3 +11,13 @@ if [ "$(grep -c third.gluster.loc /etc/hosts)" -lt 1 ]; then
 fi
 
 . /vagrant/provision/common.sh
+
+if [ ! -f "${vault_binary}" ]; then
+  echo "DOWNLOADING HASHICORP VAULT..."
+  wget -q "https://releases.hashicorp.com/vault/0.11.3/${vault_filename}"
+  echo "EXTRACTING HASHICORP VAULT..."
+  unzip "${vault_filename}" > /dev/null
+  rm "${vault_filename}"
+  echo "MOVING HASHICORP VAULT..."
+  mv ./vault "${vault_binary}"
+fi
